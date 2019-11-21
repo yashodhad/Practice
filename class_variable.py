8bc47c8447d9dd1a13bbebc7b50099e67d7e5267
yashu <yashuputta15@gmail.com>
2019-11-21 14:57:31 +0530

    new file

---
+++ class_variable.py
@@ -0,0 +1,18 @@
+# class is nothing but it is a real time entity which has state and behaviuor
+#state is nothing but it is a variable
+#Behaviour is noting but it is a method.
+"""
+syntax:
+classname.variable name
+classname.function name
+
+class variable=static variable
+class function=static function .
+"""
+class A():
+    y=10
+    @staticmethod
+    def python():
+        print("class A")
+print(A.y)
+A.python()
