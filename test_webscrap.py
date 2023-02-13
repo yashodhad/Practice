c5969e1cd8d5dd4aaf57300f597d1e5a45bf3ae8
Yashodha D <57935153+yashodhad@users.noreply.github.com>
2023-02-13 17:23:52 +0530

    Update and rename webscrap.py to test_webscrap.py

---
+++ test_webscrap.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#from openpyxl import Workbook
+from selenium.webdriver import Chrome
+import re
+import json
+import pytest
+
+def test_webscrap():
+
+  driver=Chrome('D:\\selenium_Drivers\\chromedriver.exe')
+  driver.maximize_window()
+  driver.get("https://www.amazon.in/")
+  driver.implicitly_wait(20)
+
+
+  driver.find_element(By.XPATH,"//input[contains(@id,'search')]").send_keys("iphones")
+  driver.find_element(By.XPATH,"//input[@value='Go']").click()
+  driver.find_element_by_xpath('//span[contains(text(),"Apple iPhone 14 128GB Starlight")]').click()
+
+  driver.switch_to.window(driver.window_handles[1])
+  title=driver.title
+  items=title.split(':')
+  print(items)
+
+  itemsdict={itm for itm in items}
+  d1=dict(enumerate(itemsdict))
+  print(d1)
+
+  driver.switch_to.window(driver.window_handles[0])
+  driver.close()
+
+
