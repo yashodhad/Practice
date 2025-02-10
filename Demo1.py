7b83693baa690320946f37cfd66d0af36b3e9233
Venkateshwara D <yashuputta15@gmail.com>
2025-02-10 13:35:22 +0530

    decorator file first commit

---
+++ Demo1.py
@@ -0,0 +1,18 @@
+#  A decorator is a function that takes another function as an argument and extends its behavior without modifying it.
+
+# simple example
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called")
+        func()
+        print("Something is happening after the function is called")
+    return wrapper  # Return the wrapper function itself, not the result of calling it
+
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+# using the decorator function
+say_hello()
